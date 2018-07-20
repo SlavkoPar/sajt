ba98f16c4c2ac01accbbe07aee66cdc64907a5b9
SlavkoPar <slavko.parezanin@gmail.com>
2018-07-20 08:31:05 +0200

    exclude .vscode

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
-import { configure } from 'enzyme';
 import { shallow, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
 
 import App from './App'; 
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  //const div = document.createElement('div');
   //ReactDOM.render(
   //  <BrowserRouter> 
   //    <App />
